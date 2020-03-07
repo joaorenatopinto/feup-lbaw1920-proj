b76e6f30322796fe467bd607013b23ba8718e664
Gonçalo Pereira <goncalo.valpneus@hotmail.com>
2020-03-07 23:47:42 +0000

    Started login page

---
+++ TestWireflows/login.php
@@ -0,0 +1,19 @@
+<?php include_once 'header.php' ?>
+
+
+    <div class="col justify-content-center d-flex">
+
+    <form>
+        <input type="text" id="login" class="form-control " name="login" placeholder="login">
+        <input type="text" id="password" class="form-control" name="login" placeholder="password">
+        <div class ="d-flex justify-content-around">
+            <button type="submit" class="btn btn-primary">Log In</button>
+            <button type="submit" class="btn btn-primary">Register</button>
+        </div>
+    </form>
+    </div>
+
+
+
+
+<?php include_once 'footer.php' ?>
