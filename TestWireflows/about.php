4a1338fe2bc87106321c7eda3da9061f8a00e515
Gonçalo Pereira <goncalo.valpneus@hotmail.com>
2020-03-09 18:45:48 +0000

    About page

---
+++ TestWireflows/about.php
@@ -0,0 +1,38 @@
+<?php include_once 'header.php' ?>
+
+
+<div class="d-flex mt-5 justify-content-center" >
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="/img/Risinhos.jpg" style="height: 30rem;">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="/img/Standby.jpg" style="height: 30rem;">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="/img/Patilhas.jpg" style="height: 30rem;">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="/img/Will.jpg" style="height: 30rem;">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
+<?php include_once 'footer.php' ?>
