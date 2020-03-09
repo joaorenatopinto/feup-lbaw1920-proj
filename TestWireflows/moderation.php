bef3da9b46f33b3ba082c83de5ad71a7238366c8
Pastilhas <joao.campos1221@gmail.com>
2020-03-09 17:06:18 +0000

    piças

---
+++ TestWireflows/moderation.php
@@ -0,0 +1,24 @@
+<?php include_once 'header.php' ?>
+
+<div class="container-fluid">
+  <!--  -->
+  <div class="row">
+    <div class="col-2"></div>
+    <div class="col">
+      <ul class="nav nav-tabs mt-4">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Users</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Auctions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Reports</a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-2"></div>
+  </div>
+</div>
+
+<?php include_once 'footer.php' ?>
