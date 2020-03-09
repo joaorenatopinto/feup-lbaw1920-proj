2d1832d4542b28944c246a32a031bd986a6b04dc
Pastilhas <joao.campos1221@gmail.com>
2020-03-09 16:20:10 +0000

    profile ended

--- TestWireflows/userprofile.php
+++ TestWireflows/userprofile.php
@@ -1,9 +1,11 @@
 <?php include_once 'header.php' ?>
 
 <div class="container-fluid">
-  <div class="center">
+  <!-- Image -->
+  <div class="center mt-5">
     <div class="text-center"><img src="./img/cages.png" class="rounded-circle img-fluid" alt="..."></div>
   </div>
+  <!-- Rating / Name / Age / Desc / Contact -->
   <div class="container mt-5 mx-auto">
     <div class="center row">
       <div class="text-center col">
@@ -14,7 +16,12 @@
         <div>20 years old</div>
       </div>
     </div>
-    <div class="center row mt-5">
+    <div class="center row mt-1">
+      <div class="text-center col">
+      <img src="./img/stars.png" class="img-fluid" alt="...">
+      </div>
+    </div>
+    <div class="center row mt-4">
       <div class="text-center col-sm-4">
       </div>
       <div class="text-center col-sm-4">
@@ -23,7 +30,7 @@
       <div class="text-center col-sm-4">
       </div>
     </div>
-    <div class="center row mt-5">
+    <div class="center row mt-4">
       <div class="text-center col">
         <div>Lorem@ipsum.sit</div>
       </div>
