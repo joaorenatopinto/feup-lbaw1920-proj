c2564cf335862e32e0cd001785bdabf972ce8ee1
Gonçalo Pereira <goncalo.valpneus@hotmail.com>
2020-03-09 17:46:02 +0000

    added motos

--- TestWireflows/motas.php
+++ TestWireflows/motas.php
@@ -16,7 +16,7 @@
   </div>
   <div class="card m-3" style="width: 30rem;">
     <div class="d-flex justify-content-end">
-      <img class="card-img-top" src="img/Harley-Davidson-FXDR-114_Fernando-M-1.jpg" alt="Card image cap">
+      <img class="card-img-top" src="img/yamaha.jpg" alt="Card image cap">
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -26,7 +26,7 @@
   </div>
   <div class="card m-3" style="width: 30rem;">
     <div class="d-flex justify-content-end">
-      <img class="card-img-top" src="img/Harley-Davidson-FXDR-114_Fernando-M-1.jpg" alt="Card image cap">
+      <img class="card-img-top" src="img/kawasaki.jpg" alt="Card image cap">
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
