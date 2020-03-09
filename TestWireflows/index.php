4ddb3a342349dcba9ad7b743d4db44f458e5edca
Pastilhas <joao.campos1221@gmail.com>
2020-03-09 13:55:50 +0000

    new images

--- TestWireflows/index.php
+++ TestWireflows/index.php
@@ -46,10 +46,10 @@
 <div class="container">
   <div class="row">
     <div class="col"><a href="/motas.php"><img class="d-block w-100 img-rounded img-fluid" src="img/motos.png" alt="Category Motos"></a></div>
-    <div class="col"><a href="/carros.php"><img class="d-block w-100 img-rounded img-fluid" src="img/cars.png" alt="Category Motos"></a></div>
+    <div class="col"><a href="/carros.php"><img class="d-block w-100 img-rounded img-fluid" src="img/cars.png" alt="Category Cars"></a></div>
     <div class="w-100"></div>
-    <div class="col"><a href="/eletrodomesticos.php"><img class="d-block w-100 img-rounded img-fluid" src="img/motos.png" alt="Category Motos"></a></div>
-    <div class="col"><a href="/carros.php"><img class="d-block w-100 img-rounded img-fluid" src="img/cars.png" alt="Category Motos"></a></div>
+    <div class="col"><a href="/eletrodomesticos.php"><img class="d-block w-100 img-rounded img-fluid" src="img/antiques.png" alt="Category antiques"></a></div>
+    <div class="col"><a href="/carros.php"><img class="d-block w-100 img-rounded img-fluid" src="img/phones.png" alt="Category Phones"></a></div>
   </div>
 </div>
 
