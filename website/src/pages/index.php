f3887fe8f1130c46598b89de8c6a199e26696034
João Renato Pinto <jrcp210@gmail.com>
2020-03-22 14:59:40 +0000

    Responsive pages , Index and User Profile

--- website/src/pages/index.php
+++ website/src/pages/index.php
@@ -1,60 +1,60 @@
 <?php include_once 'header_nonauth.php' ?>
-
-<div id="center" class="d-flex p-3 bd-highlight justify-content-center min-vh-25">
-  <!-- Featured Auctions Carousel -->
-  <div id="featuredAuctions" class="carousel slide w-50" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#featuredAuctions" data-slide-to="0" class="active"></li>
-      <li data-target="#featuredAuctions" data-slide-to="1"></li>
-      <li data-target="#featuredAuctions" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <a href="auction.php">
-          <img class="d-block w-100 img-rounded img-fluid" src="/img/Harley-Davidson-FXDR-114_Fernando-M-1.jpg" alt="First slide">
-        </a>
-        <div class="carousel-caption d-none d-md-block">
-          <h5 class="text-light">Harley Davidson</h5>
-          <p>2000.34€</p>
+<div class="container">
+  <div id="center" class="d-flex p-3 bd-highlight justify-content-center">
+    <!-- Featured Auctions Carousel -->
+    <div id="featuredAuctions" class="carousel slide w-100 " data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#featuredAuctions" data-slide-to="0" class="active"></li>
+        <li data-target="#featuredAuctions" data-slide-to="1"></li>
+        <li data-target="#featuredAuctions" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <a href="auction.php">
+            <img class="d-block w-100 img-rounded img-fluid" src="/img/Harley-Davidson-FXDR-114_Fernando-M-1.jpg" alt="First slide">
+          </a>
+          <div class="carousel-caption d-none d-md-block">
+            <h5 class="text-light">Harley Davidson</h5>
+            <p>2000.34€</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100 img-rounded img-fluid" src="/img/naom_5dd790964de6c.jpg" alt="Second slide">
+          <div class="carousel-caption d-none d-md-block">
+            <h5 class="text-light">Tesla</h5>
+            <p>3000.00€</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100 img-fluid" src="/img/11154232_xxl_v1557372653930.jpg" alt="Third slide">
+          <div class="carousel-caption d-none d-md-block">
+            <h5 class="text-light">Rolex</h5>
+            <p>300.00€</p>
+          </div>
         </div>
       </div>
-      <div class="carousel-item">
-        <img class="d-block w-100 img-rounded img-fluid" src="/img/naom_5dd790964de6c.jpg" alt="Second slide">
-        <div class="carousel-caption d-none d-md-block">
-          <h5 class="text-light">Tesla</h5>
-          <p>3000.00€</p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img class="d-block w-100 img-fluid" src="/img/11154232_xxl_v1557372653930.jpg" alt="Third slide">
-        <div class="carousel-caption d-none d-md-block">
-          <h5 class="text-light">Rolex</h5>
-          <p>300.00€</p>
-        </div>
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#featuredAuctions" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#featuredAuctions" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
-
-<!-- Categories -->
-<div class="d-flex p-2 bd-highlight justify-content-center">
-  <div class="container w-50 m-3">
-    <div class="row">
-      <div class="col m-2"><a href="motas.php"><img class="d-block w-100 img-rounded img-fluid" src="/img/motos.png" alt="Category Motos"></a></div>
-      <div class="col m-2"><a href="#"><img class="d-block w-100 img-rounded img-fluid" src="/img/cars.png" alt="Category Cars"></a></div>
-      <div class="w-100"></div>
-      <div class="col m-2"><a href="#"><img class="d-block w-100 img-rounded img-fluid" src="/img/antiques.png" alt="Category antiques"></a></div>
-      <div class="col m-2"><a href="#"><img class="d-block w-100 img-rounded img-fluid" src="/img/phones.png" alt="Category Phones"></a></div>
+      <a class="carousel-control-prev" href="#featuredAuctions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#featuredAuctions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
   </div>
-</div>
 
+  <!-- Categories -->
+  <div class="d-flex p-2 bd-highlight justify-content-center">
+    <div class="container w-100 m-3">
+      <div class="row">
+        <div class="col m-2"><a href="motas.php"><img class="d-block w-100 img-rounded img-fluid" src="/img/motos.png" alt="Category Motos"></a></div>
+        <div class="col m-2"><a href="#"><img class="d-block w-100 img-rounded img-fluid" src="/img/cars.png" alt="Category Cars"></a></div>
+        <div class="w-100"></div>
+        <div class="col m-2"><a href="#"><img class="d-block w-100 img-rounded img-fluid" src="/img/antiques.png" alt="Category antiques"></a></div>
+        <div class="col m-2"><a href="#"><img class="d-block w-100 img-rounded img-fluid" src="/img/phones.png" alt="Category Phones"></a></div>
+      </div>
+    </div>
+  </div>
+</div>
 <?php include_once 'footer.php' ?>
--- website/src/pages/userprofile.php
+++ website/src/pages/userprofile.php
@@ -1,108 +1,109 @@
 <?php include_once 'header.php' ?>
 
-<div class="d-flex flex-row justify-content-center">
-  <!-- Image -->
-  <div class="col-sm-5">
-    <div class="card ">
-      <div class="center mt-4">
-        <div class="text-center"><img src="../img/cages.png" class="rounded-circle img-fluid" alt="..."></div>
-      </div>
-      <!-- Rating / Name / Age / Desc / Contact -->
-      <div class="d-flex justify-content-center mt-5">
-        <div class="container mt-4 mx-auto">
-          <div class="center row">
-            <div class="text-center col">
-              <div>Kid "Colher" Cages</div>
+<div class="container">
+  <div class="row">
+    <!-- Image -->
+    <div class="col-xs-5 col-md-3">
+      <div class="card">
+        <div class="center mt-4">
+          <div class="text-center"><img src="../img/cages.png" class="rounded-circle img-fluid" alt="..."></div>
+        </div>
+        <!-- Rating / Name / Age / Desc / Contact -->
+        <div class="d-flex justify-content-center mt-5">
+          <div class="container mt-4 mx-auto">
+            <div class="center row">
+              <div class="text-center col">
+                <div>Kid "Colher" Cages</div>
+              </div>
+              <div class="w-100"></div>
+              <div class="text-center col">
+                <div>20 years old</div>
+              </div>
             </div>
-            <div class="w-100"></div>
-            <div class="text-center col">
-              <div>20 years old</div>
+            <div class="center row mt-1">
+              <div class="text-center col">
+                <img src="../img/stars.png" class="/img-fluid" alt="...">
+              </div>
             </div>
-          </div>
-          <div class="center row mt-1">
-            <div class="text-center col">
-              <img src="../img/stars.png" class="/img-fluid" alt="...">
+            <div class="center row mt-4">
+              <div class="text-center col-sm-4">
+              </div>
+              <div class="text-center col-sm-4">
+                <div>Newbie seller. Still learning the craft. Most interested in muscle cars and antique watches.</div>
+              </div>
+              <div class="text-center col-sm-4">
+              </div>
             </div>
-          </div>
-          <div class="center row mt-4">
-            <div class="text-center col-sm-4">
-            </div>
-            <div class="text-center col-sm-4">
-              <div>Newbie seller. Still learning the craft. Most interested in muscle cars and antique watches.</div>
-            </div>
-            <div class="text-center col-sm-4">
-            </div>
-          </div>
-          <div class="center row mt-4">
-            <div class="text-center col">
-              <div>kidcages@gmail.com</div>
+            <div class="center row mt-4">
+              <div class="text-center col">
+                <div>kidcages@gmail.com</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
+      </div>
+    </div>
+    <div class="col-xs-9 col-md-9">
+      <div class="card m-3 mx-auto">
+        <div class="d-flex justify-content-end">
+          <img class="card-img-top w-50" src="/img/Harley-Davidson-FXDR-114_Fernando-M-1.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Harley Davidson</h5>
+            <p class="card-text">2008, 1600cc, 6vel, many extras and in immaculate condition. Recent overhaul and new
+              tires.
+            </p>
+            <p class="card-text">00d 22h 25m 17s left</p>
+            <a href="auction.php" class="btn btn-primary" style="width: 10rem;">BID NOW</a>
+          </div>
+        </div>
+      </div>
+      <div class="card m-3 mx-auto">
+        <div class="d-flex justify-content-end">
+          <img class="card-img-top w-50" src="/img/yamaha.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Yamaha R10M</h5>
+            <p class="card-text">2018, 1600cc, 5vel, many extras and new tires. Recent overhaul.</p>
+            <p class="card-text">10d 20h 35m 12s left</p>
+            <a href="#" class="btn btn-primary" style="width: 10rem;">BID NOW</a>
+          </div>
+        </div>
+      </div>
+      <div class="card m-3 mx-auto">
+        <div class="d-flex justify-content-end">
+          <img class="card-img-top w-50" src="/img/yamaha.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Yamaha R10M</h5>
+            <p class="card-text">2018, 1600cc, 5vel, many extras and new tires. Recent overhaul.</p>
+            <p class="card-text">10d 20h 35m 12s left</p>
+            <a href="#" class="btn btn-primary" style="width: 10rem;">BID NOW</a>
+          </div>
+        </div>
+      </div>
+      <div class="card m-3 mx-auto">
+        <div class="d-flex justify-content-end">
+          <img class="card-img-top w-50" src="/img/yamaha.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Yamaha R10M</h5>
+            <p class="card-text">2018, 1600cc, 5vel, many extras and new tires. Recent overhaul.</p>
+            <p class="card-text">10d 20h 35m 12s left</p>
+            <a href="#" class="btn btn-primary" style="width: 10rem;">BID NOW</a>
+          </div>
+        </div>
+      </div>
+      <div class="card m-3 mx-auto">
+        <div class="d-flex justify-content-end">
+          <img class="card-img-top w-50" src="/img/yamaha.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Yamaha R10M</h5>
+            <p class="card-text">2018, 1600cc, 5vel, many extras and new tires. Recent overhaul.</p>
+            <p class="card-text">10d 20h 35m 12s left</p>
+            <a href="#" class="btn btn-primary" style="width: 10rem;">BID NOW</a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-  <div>
-    <div class="card m-3 mx-auto">
-      <div class="d-flex justify-content-end">
-        <img class="card-img-top w-50" src="/img/Harley-Davidson-FXDR-114_Fernando-M-1.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Harley Davidson</h5>
-          <p class="card-text">2008, 1600cc, 6vel, many extras and in immaculate condition. Recent overhaul and new
-            tires.
-          </p>
-          <p class="card-text">00d 22h 25m 17s left</p>
-          <a href="auction.php" class="btn btn-primary" style="width: 10rem;">BID NOW</a>
-        </div>
-      </div>
-    </div>
-    <div class="card m-3 mx-auto">
-      <div class="d-flex justify-content-end">
-        <img class="card-img-top w-50" src="/img/yamaha.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Yamaha R10M</h5>
-          <p class="card-text">2018, 1600cc, 5vel, many extras and new tires. Recent overhaul.</p>
-          <p class="card-text">10d 20h 35m 12s left</p>
-          <a href="#" class="btn btn-primary" style="width: 10rem;">BID NOW</a>
-        </div>
-      </div>
-    </div>
-    <div class="card m-3 mx-auto">
-      <div class="d-flex justify-content-end">
-        <img class="card-img-top w-50" src="/img/yamaha.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Yamaha R10M</h5>
-          <p class="card-text">2018, 1600cc, 5vel, many extras and new tires. Recent overhaul.</p>
-          <p class="card-text">10d 20h 35m 12s left</p>
-          <a href="#" class="btn btn-primary" style="width: 10rem;">BID NOW</a>
-        </div>
-      </div>
-    </div>
-    <div class="card m-3 mx-auto">
-      <div class="d-flex justify-content-end">
-        <img class="card-img-top w-50" src="/img/yamaha.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Yamaha R10M</h5>
-          <p class="card-text">2018, 1600cc, 5vel, many extras and new tires. Recent overhaul.</p>
-          <p class="card-text">10d 20h 35m 12s left</p>
-          <a href="#" class="btn btn-primary" style="width: 10rem;">BID NOW</a>
-        </div>
-      </div>
-    </div>
-    <div class="card m-3 mx-auto">
-      <div class="d-flex justify-content-end">
-        <img class="card-img-top w-50" src="/img/yamaha.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Yamaha R10M</h5>
-          <p class="card-text">2018, 1600cc, 5vel, many extras and new tires. Recent overhaul.</p>
-          <p class="card-text">10d 20h 35m 12s left</p>
-          <a href="#" class="btn btn-primary" style="width: 10rem;">BID NOW</a>
-        </div>
-      </div>
-    </div>
-  </div>
+  
 </div>
-</div>
-
 <?php include_once 'footer.php' ?>
