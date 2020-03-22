6d569119b42f56d23c7677cb8fdcace8c4a44a19
João Renato Pinto <jrcp210@gmail.com>
2020-03-22 17:17:39 +0000

    Category page is responsive

--- website/src/pages/motas.php
+++ website/src/pages/motas.php
@@ -1,39 +1,40 @@
 <?php include_once 'header.php' ?>
 
+<div class="container">
+  <div class="d-flex justify-content-center mt-5">
+    <div>
+      <div class="card mx-auto m-3">
+        <div class="row no-gutters">
+          <img class="card-img col-md-5" src="/img/Harley-Davidson-FXDR-114_Fernando-M-1.jpg" alt="Card image cap">
+          <div class="card-body col-md-7 p-3">
+            <h5 class="card-title">Harley Davidson</h5>
+            <p class="card-text">2008, 1600cc, 6vel, many extras and in immaculate condition. Recent overhaul and new tires.</p>
+            <p class="card-text">00d 22h 25m 17s left</p>
+            <h3><a href="auction.php" class="btn btn-primary" style="width: 10rem;">BID NOW</a> <span class="badge">2000.43€</span></h3>
+          </div>
+        </div>
 
-<div class="d-flex justify-content-center mt-5">
-  <div>
-    <div class="card m-3 w-50 mx-auto">
-      <div class="d-flex justify-content-end">
-        <img class="card-img-top w-50" src="/img/Harley-Davidson-FXDR-114_Fernando-M-1.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Harley Davidson</h5>
-          <p class="card-text">2008, 1600cc, 6vel, many extras and in immaculate condition. Recent overhaul and new tires.</p>
-          <p class="card-text">00d 22h 25m 17s left</p>
-          <h3><a href="auction.php" class="btn btn-primary" style="width: 10rem;">BID NOW</a> <span class="badge">2000.43€</span></h3>
+      </div>
+      <div class="card mx-auto m-3">
+        <div class="row no-gutters">
+          <img class="card-img col-md-5" src="/img/yamaha.jpg" alt="Card image cap">
+          <div class="card-body col-md-7 p-3">
+            <h5 class="card-title">Yamaha R10M</h5>
+            <p class="card-text">2018, 1600cc, 5vel, many extras and new tires. Recent overhaul.</p>
+            <p class="card-text">10d 20h 35m 12s left</p>
+            <h3><a href="auction.php" class="btn btn-primary" style="width: 10rem;">BID NOW</a> <span class="badge">20.43€</span></h3>
+          </div>
         </div>
       </div>
-
-    </div>
-    <div class="card m-3 w-50 mx-auto">
-      <div class="d-flex justify-content-end">
-        <img class="card-img-top w-50" src="/img/yamaha.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Yamaha R10M</h5>
-          <p class="card-text">2018, 1600cc, 5vel, many extras and new tires. Recent overhaul.</p>
-          <p class="card-text">10d 20h 35m 12s left</p>
-          <h3><a href="auction.php" class="btn btn-primary" style="width: 10rem;">BID NOW</a> <span class="badge">20.43€</span></h3>
-        </div>
-      </div>
-    </div>
-    <div class="card m-3 w-50 mx-auto">
-      <div class="d-flex justify-content-end">
-        <img class="card-img-top w-50" src="/img/kawasaki.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Kawasaki Ninja</h5>
-          <p class="card-text">2015, 1600cc, 7vel, next-level technology advancements and sharp styling updates.</p>
-          <p class="card-text">03d 02h 21m 57s left</p>
-          <h3><a href="auction.php" class="btn btn-primary" style="width: 10rem;">BID NOW</a> <span class="badge">1337.69€</span></h3>
+      <div class="card mx-auto m-3">
+        <div class="row no-gutters">
+          <img class="card-img col-md-5" src="/img/kawasaki.jpg" alt="Card image cap">
+          <div class="card-body col-md-7 p-3">
+            <h5 class="card-title">Kawasaki Ninja</h5>
+            <p class="card-text">2015, 1600cc, 7vel, next-level technology advancements and sharp styling updates.</p>
+            <p class="card-text">03d 02h 21m 57s left</p>
+            <h3><a href="auction.php" class="btn btn-primary" style="width: 10rem;">BID NOW</a> <span class="badge">1337.69€</span></h3>
+          </div>
         </div>
       </div>
     </div>
