b6ce4819e3561cf1b0695a091d54ef58258055c4
leonardofmoura <leofmoura17@gmail.com>
2020-03-22 20:49:13 +0000

    Added money deposit and extraction buttons

--- website/src/pages/header.php
+++ website/src/pages/header.php
@@ -21,7 +21,25 @@
         <div class="d-flex flex-row order-2 order-lg-3">
           <ul class="navbar-nav flex-row">
             <li class="nav-item">
-              <a class="nav-link text-light" href="#">7.75€</a>
+              <a class="nav-link text-light" data-toggle="modal" data-target="#moneyModal">7.75€</a>
+
+              <!-- Modal -->
+              <div class="modal fade" id="moneyModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLongTitle">Money Management</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      <button class="btn btn-block btn-danger">Deposit Money</button>
+                      <button class="btn btn-block btn-outline-danger">Extract Money</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </li>
             <li class="nav-item">
               <a class="nav-link text-light" href="userprofile.php">João Cages</a>
