27f8572c383c5e31ae628d2310435c5c4142103d
João Renato Pinto <jrcp210@gmail.com>
2020-03-28 17:14:54 +0000

    Changes applied to create script

--- database/create.sql
+++ database/create.sql
@@ -1,14 +1,11 @@
-DROP TYPE IF EXISTS Stars;
-CREATE TYPE Stars AS ENUM ('0','1','2','3','4','5');
-
 DROP TYPE IF EXISTS user_status CASCADE;
 CREATE TYPE user_status AS ENUM ('active', 'moderator', 'banned', 'recoMod');
 
 DROP TYPE IF EXISTS auction_status CASCADE;
 CREATE TYPE auction_status AS ENUM ('ongoing', 'removed', 'closed');
 
-DROP TYPE IF EXISTS notification_status CASCADE;
-CREATE TYPE notification_status AS ENUM ('notSeen', 'seen');
+DROP TYPE IF EXISTS notification_type CASCADE;
+CREATE TYPE notification_status AS ENUM ('auction_ended', 'auction_won', 'new_bid', 'outdated_bid');
 
 DROP TYPE IF EXISTS report_status CASCADE;
 CREATE TYPE report_status AS ENUM ('notSeen', 'seen', 'closed');
@@ -22,17 +19,17 @@ name VARCHAR UNIQUE NOT NULL
 DROP TABLE IF EXISTS auctionStatus CASCADE;
 CREATE TABLE auctionStatus (
 id SERIAL PRIMARY KEY,
-TYPE auction_status NOT NULL DEFAULT 'ongoing',
+status auction_status NOT NULL DEFAULT 'ongoing',
 dateChanged DATE DEFAULT now(),
-oldStatus VARCHAR
+auction INTEGER NOT NULL REFERENCES auction(id)
 );
 
 DROP TABLE IF EXISTS userStatus CASCADE;
 CREATE TABLE userStatus (
 id SERIAL PRIMARY KEY,
-TYPE user_status NOT NULL DEFAULT 'active',
+status user_status NOT NULL DEFAULT 'active',
 dateChanged DATE DEFAULT now(),
-oldStatus VARCHAR
+user INTEGER NOT NULL REFERENCES "user"(id)
 );
 
 
@@ -45,13 +42,13 @@ CREATE TABLE "image" (
 
 DROP TABLE IF EXISTS "user" CASCADE;
 CREATE TABLE "user" (
-    username VARCHAR PRIMARY KEY,
+    id SERIAL PRIMARY KEY
+    username VARCHAR NOT NULL,
     password VARCHAR NOT NULL,
     name VARCHAR NOT NULL,
     email VARCHAR UNIQUE NOT NULL,
-    nif INTEGER  UNIQUE NOT NULL,
+    nif VARCHAR  UNIQUE NOT NULL,
     image_id INTEGER UNIQUE NOT NULL REFERENCES "image"(id),
-    status_id INTEGER UNIQUE NOT NULL REFERENCES userStatus(id)
 );
 
 DROP TABLE IF EXISTS auction CASCADE;
@@ -63,33 +60,35 @@ CREATE TABLE auction (
     closeDate DATE NOT NULL CHECK (closeDate  > startDate),
     initialValue INTEGER NOT NULL CHECK (initialValue > 0),
     category_id INTEGER NOT NULL REFERENCES category(id),
-    status_id INTEGER NOT NULL REFERENCES auctionStatus(id),
-    owner VARCHAR NOT NULL REFERENCES "user"(username)
+    owner INTEGER NOT NULL REFERENCES "user"(id)
 );
 
 DROP TABLE IF EXISTS "transaction";
 CREATE TABLE "transaction" (
     id SERIAL PRIMARY KEY,
-    value NUMERIC  NOT NULL CHECK (value != 0),
+    value NUMERIC  NOT NULL CHECK (value > 0),
     date DATE NOT NULL DEFAULT now(),
     description VARCHAR NOT NULL,
-    username VARCHAR UNIQUE NOT NULL REFERENCES "user"(username)
+    sender_id INTEGER NOT NULL REFERENCES "user"(id)
+    receiver_id INTEGER NOT NULL REFERENCES "user"(id) CHECK (NOT (sender_id == NULL AND receiver_id == NULL)),
+    is_reserved BOOLEAN NOT NULL,
+    auction INTEGER REFERENCES auction(id)
 );
 
 DROP TABLE IF EXISTS follows;
 CREATE TABLE follows (
-    username VARCHAR UNIQUE NOT NULL REFERENCES "user"(username),
+    user VARCHAR UNIQUE NOT NULL REFERENCES "user"(id),
     category_id INTEGER UNIQUE NOT NULL REFERENCES category(id)
 );
 
 DROP TABLE IF EXISTS followsAuction;
 CREATE TABLE followsAuction(
-    username VARCHAR UNIQUE NOT NULL REFERENCES "user"(username),
+    user VARCHAR UNIQUE NOT NULL REFERENCES "user"(id),
     auction_id INTEGER UNIQUE NOT NULL REFERENCES auction(id)
 );
 
-DROP TABLE IF EXISTS hasImage;
-CREATE TABLE hasImage (
+DROP TABLE IF EXISTS AuctionImage;
+CREATE TABLE AuctionImage (
     auction_id INTEGER UNIQUE NOT NULL REFERENCES auction(id),
     image_id INTEGER UNIQUE NOT NULL REFERENCES "image"(id)
 );
@@ -100,16 +99,16 @@ CREATE TABLE bid (
     value INTEGER NOT NULL CHECK (value > 0),
     date DATE NOT NULL DEFAULT now(),
     auction_id INTEGER NOT NULL REFERENCES auction(id),
-    username VARCHAR NOT NULL REFERENCES "user"(username)
+    user VARCHAR NOT NULL REFERENCES "user"(id)
 );
 
 DROP TABLE IF EXISTS review;
 CREATE TABLE review (
     id SERIAL PRIMARY KEY,
-    stars INTEGER NOT NULL CHECK ((stars >=0) AND (stars <= 5)),
+    stars INTEGER NOT NULL CHECK ((stars >= 0) AND (stars <= 5)),
     description VARCHAR NOT NULL,
     auction_id INTEGER NOT NULL REFERENCES auction(id),
-    username VARCHAR NOT NULL REFERENCES "user"(username)
+    user VARCHAR NOT NULL REFERENCES "user"(id)
 );
 
 DROP TABLE IF EXISTS reportStatus CASCADE;
@@ -126,7 +125,7 @@ CREATE TABLE report (
     id SERIAL PRIMARY KEY,
     description VARCHAR NOT NULL,
     auction_id INTEGER NOT NULL REFERENCES auction(id),
-    username VARCHAR NOT NULL REFERENCES "user"(username),
+    user VARCHAR NOT NULL REFERENCES "user"(id),
     status_id INTEGER NOT NULL REFERENCES reportStatus(id)
 );
 
@@ -134,7 +133,7 @@ DROP TABLE IF EXISTS bugReport;
 CREATE TABLE bugReport (
     id SERIAL PRIMARY KEY,
     description VARCHAR NOT NULL,
-    username VARCHAR NOT NULL REFERENCES "user"(username),
+    user VARCHAR NOT NULL REFERENCES "user"(id),
     status_id INTEGER NOT NULL REFERENCES reportStatus(id)
 );
 
@@ -144,18 +143,13 @@ CREATE TABLE "admin" (
     password VARCHAR NOT NULL
 );
 
-DROP TABLE IF EXISTS notificationStatus CASCADE;
-CREATE TABLE notificationStatus (
-    id SERIAL PRIMARY KEY,
-    TYPE notification_status NOT NULL DEFAULT 'notSeen',
-    dateChanged DATE DEFAULT now()
-);
 
 DROP TABLE IF EXISTS "notification";
 CREATE TABLE "notification"(
     id SERIAL PRIMARY KEY,
     title VARCHAR NOT NULL,
     date DATE NOT NULL DEFAULT now(),
-    username VARCHAR NOT NULL REFERENCES "user"(username),
-    status_id INTEGER NOT NULL REFERENCES notificationStatus(id)
+    user VARCHAR NOT NULL REFERENCES "user"(id),
+    seen BOOLEAN NOT NULL DEFAULT false,
+    type notification_type NOT NULL
 );
