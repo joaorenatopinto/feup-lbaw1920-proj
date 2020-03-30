2fdcd420666ca52881b0372e426caa9d4f0bbce5
João Renato Pinto <jrcp210@gmail.com>
2020-03-31 00:00:36 +0100

    A6

--- database/create.sql
+++ database/create.sql
@@ -5,7 +5,7 @@ DROP TYPE IF EXISTS auction_status CASCADE;
 CREATE TYPE auction_status AS ENUM ('ongoing', 'removed', 'closed');
 
 DROP TYPE IF EXISTS notification_type CASCADE;
-CREATE TYPE notification_status AS ENUM ('auction_ended', 'auction_won', 'new_bid', 'outdated_bid');
+CREATE TYPE notification_type AS ENUM ('auction_ended', 'auction_won', 'new_bid', 'outdated_bid');
 
 DROP TYPE IF EXISTS report_status CASCADE;
 CREATE TYPE report_status AS ENUM ('notSeen', 'seen', 'closed');
@@ -14,22 +14,6 @@ DROP TABLE IF EXISTS category CASCADE;
 CREATE TABLE category (
 id SERIAL PRIMARY KEY,
 name VARCHAR UNIQUE NOT NULL
-);
-
-DROP TABLE IF EXISTS auctionStatus CASCADE;
-CREATE TABLE auctionStatus (
-id SERIAL PRIMARY KEY,
-status auction_status NOT NULL DEFAULT 'ongoing',
-dateChanged DATE DEFAULT now(),
-auction INTEGER NOT NULL REFERENCES auction(id)
-);
-
-DROP TABLE IF EXISTS userStatus CASCADE;
-CREATE TABLE userStatus (
-id SERIAL PRIMARY KEY,
-status user_status NOT NULL DEFAULT 'active',
-dateChanged DATE DEFAULT now(),
-user INTEGER NOT NULL REFERENCES "user"(id)
 );
 
 
@@ -42,13 +26,23 @@ CREATE TABLE "image" (
 
 DROP TABLE IF EXISTS "user" CASCADE;
 CREATE TABLE "user" (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
     username VARCHAR NOT NULL,
     password VARCHAR NOT NULL,
     name VARCHAR NOT NULL,
     email VARCHAR UNIQUE NOT NULL,
+    balance NUMERIC NOT NULL DEFAULT 0,
     nif VARCHAR  UNIQUE NOT NULL,
     image_id INTEGER UNIQUE NOT NULL REFERENCES "image"(id),
+    description VARCHAR
+);
+
+DROP TABLE IF EXISTS userStatus CASCADE;
+CREATE TABLE userStatus (
+id SERIAL PRIMARY KEY,
+status user_status NOT NULL DEFAULT 'active',
+dateChanged DATE DEFAULT now(),
+user_id INTEGER NOT NULL REFERENCES "user"(id)
 );
 
 DROP TABLE IF EXISTS auction CASCADE;
@@ -60,7 +54,15 @@ CREATE TABLE auction (
     closeDate DATE NOT NULL CHECK (closeDate  > startDate),
     initialValue INTEGER NOT NULL CHECK (initialValue > 0),
     category_id INTEGER NOT NULL REFERENCES category(id),
-    owner INTEGER NOT NULL REFERENCES "user"(id)
+    user_id INTEGER NOT NULL REFERENCES "user"(id)
+);
+
+DROP TABLE IF EXISTS auctionStatus CASCADE;
+CREATE TABLE auctionStatus (
+id SERIAL PRIMARY KEY,
+status auction_status NOT NULL DEFAULT 'ongoing',
+dateChanged DATE DEFAULT now(),
+auction INTEGER NOT NULL REFERENCES auction(id)
 );
 
 DROP TABLE IF EXISTS "transaction";
@@ -69,37 +71,37 @@ CREATE TABLE "transaction" (
     value NUMERIC  NOT NULL CHECK (value > 0),
     date DATE NOT NULL DEFAULT now(),
     description VARCHAR NOT NULL,
-    sender_id INTEGER NOT NULL REFERENCES "user"(id)
-    receiver_id INTEGER NOT NULL REFERENCES "user"(id) CHECK (NOT (sender_id == NULL AND receiver_id == NULL)),
+    sender_id INTEGER NOT NULL REFERENCES "user"(id),
+    receiver_id INTEGER NOT NULL REFERENCES "user"(id) CHECK (NOT (sender_id IS NULL AND receiver_id IS NULL)),
     is_reserved BOOLEAN NOT NULL,
     auction INTEGER REFERENCES auction(id)
 );
 
 DROP TABLE IF EXISTS follows;
 CREATE TABLE follows (
-    user VARCHAR UNIQUE NOT NULL REFERENCES "user"(id),
-    category_id INTEGER UNIQUE NOT NULL REFERENCES category(id)
+    user_id INTEGER  NOT NULL REFERENCES "user"(id),
+    category_id INTEGER  NOT NULL REFERENCES category(id)
 );
 
-DROP TABLE IF EXISTS followsAuction;
-CREATE TABLE followsAuction(
-    user VARCHAR UNIQUE NOT NULL REFERENCES "user"(id),
-    auction_id INTEGER UNIQUE NOT NULL REFERENCES auction(id)
+DROP TABLE IF EXISTS follows_auction;
+CREATE TABLE follows_auction(
+    user_id INTEGER  NOT NULL REFERENCES "user"(id),
+    auction_id INTEGER  NOT NULL REFERENCES auction(id)
 );
 
-DROP TABLE IF EXISTS AuctionImage;
-CREATE TABLE AuctionImage (
+DROP TABLE IF EXISTS auction_image;
+CREATE TABLE auction_image (
     auction_id INTEGER UNIQUE NOT NULL REFERENCES auction(id),
     image_id INTEGER UNIQUE NOT NULL REFERENCES "image"(id)
 );
 
-DROP TABLE IF EXISTS bid;
+DROP TABLE IF EXISTS bid CASCADE;
 CREATE TABLE bid (
     id SERIAL PRIMARY KEY,
     value INTEGER NOT NULL CHECK (value > 0),
     date DATE NOT NULL DEFAULT now(),
     auction_id INTEGER NOT NULL REFERENCES auction(id),
-    user VARCHAR NOT NULL REFERENCES "user"(id)
+    user_id INTEGER NOT NULL REFERENCES "user"(id)
 );
 
 DROP TABLE IF EXISTS review;
@@ -108,7 +110,7 @@ CREATE TABLE review (
     stars INTEGER NOT NULL CHECK ((stars >= 0) AND (stars <= 5)),
     description VARCHAR NOT NULL,
     auction_id INTEGER NOT NULL REFERENCES auction(id),
-    user VARCHAR NOT NULL REFERENCES "user"(id)
+    user_id INTEGER NOT NULL REFERENCES "user"(id)
 );
 
 DROP TABLE IF EXISTS reportStatus CASCADE;
@@ -125,7 +127,7 @@ CREATE TABLE report (
     id SERIAL PRIMARY KEY,
     description VARCHAR NOT NULL,
     auction_id INTEGER NOT NULL REFERENCES auction(id),
-    user VARCHAR NOT NULL REFERENCES "user"(id),
+    user_id INTEGER NOT NULL REFERENCES "user"(id),
     status_id INTEGER NOT NULL REFERENCES reportStatus(id)
 );
 
@@ -133,14 +135,14 @@ DROP TABLE IF EXISTS bugReport;
 CREATE TABLE bugReport (
     id SERIAL PRIMARY KEY,
     description VARCHAR NOT NULL,
-    user VARCHAR NOT NULL REFERENCES "user"(id),
+    user_id INTEGER NOT NULL REFERENCES "user"(id),
     status_id INTEGER NOT NULL REFERENCES reportStatus(id)
 );
 
 DROP TABLE IF EXISTS "admin";
 CREATE TABLE "admin" (
     id SERIAL PRIMARY KEY,
-    username VARCHAR,
+    username VARCHAR NOT NULL,
     password VARCHAR NOT NULL
 );
 
@@ -148,9 +150,102 @@ CREATE TABLE "admin" (
 DROP TABLE IF EXISTS "notification";
 CREATE TABLE "notification"(
     id SERIAL PRIMARY KEY,
-    title VARCHAR NOT NULL,
     date DATE NOT NULL DEFAULT now(),
-    user VARCHAR NOT NULL REFERENCES "user"(id),
+    user_id INTEGER NOT NULL REFERENCES "user"(id),
     seen BOOLEAN NOT NULL DEFAULT false,
-    type notification_type NOT NULL
+    type notification_type NOT NULL,
+    bid_id  INTEGER REFERENCES bid(id) ON DELETE CASCADE CHECK ((bid_id IS NOT NULL) OR (type NOT IN ('new_bid','outdated_bid'))),
+    auction_id INTEGER REFERENCES auction(id)
 );
+
+------------- INDEXES -------------
+
+CREATE INDEX username_user ON "user" USING hash (username);
+
+CREATE INDEX category_id_auction ON auction USING hash (category_id);
+
+CREATE INDEX user_id_follows ON follows USING hash (user_id);
+
+CREATE INDEX user_id_follows_auction ON follows_auction USING hash (user_id);
+
+CREATE INDEX auction_text_search ON auction USING GIST (to_tsvector('english', title || ' ' || description));
+
+------------- TRIGGERS -------------
+
+DROP FUNCTION IF EXISTS user_balance_insert();
+DROP TRIGGER IF EXISTS user_balance_insert ON "transaction";
+
+CREATE FUNCTION user_balance_insert() RETURNS TRIGGER AS 
+$BODY$
+BEGIN
+    IF EXISTS (SELECT * FROM "transaction" WHERE sender_id = NEW.id) THEN 
+        IF EXISTS (SELECT * FROM transaction WHERE sender_id = NEW.id) THEN 
+            UPDATE "user" SET balance = (SELECT SUM(value) FROM "transaction" WHERE sender_id = NEW.id) - (SELECT SUM(value) FROM transaction WHERE sender_id = NEW.id);
+        ELSE 
+            UPDATE "user" SET balance = (SELECT SUM(value) FROM "transaction" WHERE sender_id = NEW.id);  
+        END IF;
+    END IF;
+RETURN NEW;
+END
+$BODY$
+LANGUAGE plpgsql;
+CREATE TRIGGER user_balance_insert
+AFTER INSERT ON "transaction"
+FOR EACH ROW EXECUTE PROCEDURE user_balance_insert();
+
+DROP FUNCTION IF EXISTS user_balance_delete();
+DROP TRIGGER IF EXISTS user_balance_delete ON "transaction";
+
+CREATE FUNCTION user_balance_delete() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    IF EXISTS (SELECT * FROM "transaction" WHERE sender_id = OLD.id) THEN 
+        IF EXISTS (SELECT * FROM transaction WHERE sender_id = OLD.id) THEN 
+            UPDATE "user" SET balance = (SELECT SUM(value) FROM "transaction" WHERE sender_id = OLD.id) - (SELECT SUM(value) FROM transaction WHERE sender_id = OLD.id);
+        ELSE 
+            UPDATE "user" SET balance = (SELECT SUM(value) FROM "transaction" WHERE sender_id = OLD.id);  
+        END IF;
+    END IF;
+RETURN NEW;
+END
+$BODY$
+LANGUAGE plpgsql;
+CREATE TRIGGER user_balance_delete
+AFTER DELETE ON "transaction"
+FOR EACH ROW EXECUTE PROCEDURE user_balance_delete();
+
+
+DROP FUNCTION IF EXISTS notification_on_bid();
+DROP TRIGGER IF EXISTS notification_on_bid ON bid;
+
+CREATE FUNCTION notification_on_bid() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+IF EXISTS (SELECT * FROM bid WHERE auction_id = NEW.auction_id) THEN
+    INSERT INTO notification (type,auction_id,user_id,bid_id) 
+        VALUES ('outdated_bid', NEW.auction_id, (SELECT bid.user_id FROM bid WHERE bid.auction_id = NEW.auction_id AND bid.value = (SELECT MIN(value) FROM bid WHERE bid.auction_id = NEW.auction_id)), NEW.id); 
+    DELETE FROM bid WHERE auction_id = NEW.auction_id AND value < NEW.value;
+END IF;
+INSERT INTO notification (type,auction_id,user_id,bid_id) VALUES ('new_bid', NEW.auction_id, (SELECT auction.user_id FROM auction WHERE auction.id = NEW.auction_id), NEW.id);
+RETURN NEW;
+END
+$BODY$
+LANGUAGE plpgsql;
+CREATE TRIGGER notification_on_bid
+AFTER INSERT ON bid
+FOR EACH ROW EXECUTE PROCEDURE notification_on_bid();
+
+DROP FUNCTION IF EXISTS bid_deleted();
+DROP TRIGGER IF EXISTS bid_deleted ON bid;
+
+CREATE FUNCTION bid_deleted() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+DELETE FROM "transaction" WHERE sender_id = OLD.user_id AND value = OLD.value AND date = OLD.date AND is_reserved = 'true';
+RETURN NEW;
+END
+$BODY$
+LANGUAGE plpgsql;
+CREATE TRIGGER bid_deleted
+AFTER DELETE ON bid
+FOR EACH ROW EXECUTE PROCEDURE bid_deleted();
--- database/populate.sql
+++ database/populate.sql
@@ -1,85 +1,82 @@
-INSERT INTO  "category" ("name") VALUES ("Cars");
-INSERT INTO  "category" ("name") VALUES ("Motorcycles");
-INSERT INTO  "category" ("name") VALUES ("Antiques");
-INSERT INTO  "category" ("name") VALUES ("Phones"); 
-INSERT INTO  "category" ("name") VALUES ("Computers");
 
-INSERT INTO "user" (username, "password", "name", email, nif, image_id, description) VALUES ("poncho", "hash256", "Gabe Itches", "gabe.itches@lbaw.fun", "271654321", 1, "");
-INSERT INTO "user" (username, "password", "name", email, nif, image_id, description) VALUES ("appealpresent", "hash256", "Hugh Jass", "hjass1999@yahoo.com" "271456456", 2, "write here");
-INSERT INTO "user" (username, "password", "name", email, nif, image_id, description) VALUES ("aquafeeling", "hash256", "Moe Lester", "mlester98@hotmail.com", "271353268", 3, "");
-INSERT INTO "user" (username, "password", "name", email, nif, image_id, description) VALUES ("goose", "hash256", "Daniel G", "up201602314@fe.up.pt", "271536984", 4, "yes");
-INSERT INTO "user" (username, "password", "name", email, nif, image_id, description) VALUES ("kaliasoon", "hash256", "Kalia Soon", "up201642314@fe.up.pt", "271536184", 5, "yes");
+    INSERT INTO  "category" ("name") VALUES ('Cars');
+    INSERT INTO  "category" ("name") VALUES ('Motorcycles');
+    INSERT INTO  "category" ("name") VALUES ('Antiques');
+    INSERT INTO  "category" ("name") VALUES ('Phones'); 
+    INSERT INTO  "category" ("name") VALUES ('Computers');
 
-INSERT INTO auction (title, description, closeDate, initialValue, category_id, "owner") VALUES ("Mansão de luxo", "Tem portas",'2021-02-17 10:00:00', 2, 1)
-INSERT INTO auction (title, description, closeDate, initialValue, category_id, "owner") VALUES ("Dildo Ultra Dragon X", "Larga o PePINo na saladeira, agora está na hora do Ultra Dragon X",'2021-02-17 16:45:00', 2, 1)
-INSERT INTO auction (title, description, closeDate, initialValue, category_id, "owner") VALUES ("skin P90 CS:GO", "Tem cores e stickers e formas",'2021-02-17 09:41:14', 2, 1)
-INSERT INTO auction (title, description, closeDate, initialValue, category_id, "owner") VALUES ("Relogio CASIO", "Tem horas? Já não vai precisar de fazer tal pergunta com o novo Relógio CASIO",'2020-04-20 13:15:00', 2, 1)
-INSERT INTO auction (title, description, closeDate, initialValue, category_id, "owner") VALUES ("Ganda Bicha", "Um cabo que serve para fazer variar a tensão da valuma da vela grande :oksign:",'2021-02-17 09:00:15', 2, 1)
+    INSERT INTO "image" (path, alt) VALUES ('/website/src/img/Patilhas.jpg'   , 'Patilhas');
+    INSERT INTO "image" (path, alt) VALUES ('/website/src/img/Risinhos.jpg'   , 'Risinhos');
+    INSERT INTO "image" (path, alt) VALUES ('/website/src/img/Will.jpg'       , 'Will');
+    INSERT INTO "image" (path, alt) VALUES ('/website/src/img/Standby.jpg'    , 'Standby');
+    INSERT INTO "image" (path, alt) VALUES ('/website/src/img/motos.jpg'   , 'motos');
+    INSERT INTO "image" (path, alt) VALUES ('/website/src/img/antiques.jpg'   , 'antiques');
+    INSERT INTO "image" (path, alt) VALUES ('/website/src/img/phones.jpg'   , 'phones');
 
-INSERT INTO auctionStatus (status, dateChanged, auction) VALUES ('ongoing', '2017-04-17 19:41:14', 1);
-INSERT INTO auctionStatus (status, dateChanged, auction) VALUES ('ongoing', '2017-02-15 09:11:11', 2);
-INSERT INTO auctionStatus (status, dateChanged, auction) VALUES ('closed' , '2017-05-17 07:41:14', 3);
-INSERT INTO auctionStatus (status, dateChanged, auction) VALUES ('removed', '2017-09-18 11:23:00', 4);
-INSERT INTO auctionStatus (status, dateChanged, auction) VALUES ('ongoing', '2018-02-17 09:41:14', 5);
+    INSERT INTO "user" (username, "password", "name", email, nif, image_id, description) VALUES ('poncho', 'hash256', 'Gabe Itches', 'gabe.itches@lbaw.fun', '271654321', 1, 'r');
+    INSERT INTO "user" (username, "password", "name", email, nif, image_id, description) VALUES ('appealpresent', 'hash256', 'Hugh Jass', 'hjass1999@yahoo.com', '271456456', 2, 'write here');
+    INSERT INTO "user" (username, "password", "name", email, nif, image_id, description) VALUES ('aquafeeling', 'hash256', 'Moe Lester', 'mlester98@hotmail.com', '271353268', 3, 'NO');
+    INSERT INTO "user" (username, "password", "name", email, nif, image_id, description) VALUES ('goose', 'hash256', 'Daniel G', 'up201602314@fe.up.pt', '271536984', 4, 'yes');
+    INSERT INTO "user" (username, "password", "name", email, nif, image_id, description) VALUES ('kaliasoon', 'hash256', 'Kalia Soon', 'up201642314@fe.up.pt', '271536184', 5, 'yes');
 
-INSERT INTO userStatus (user_status, dateChanged, user) VALUES ('active'   , '2017-02-17 09:41:14', 1);
-INSERT INTO userStatus (user_status, dateChanged, user) VALUES ('active'   , '2017-02-17 09:41:14', 2);
-INSERT INTO userStatus (user_status, dateChanged, user) VALUES ('moderator', '2017-02-17 09:41:14', 3);
-INSERT INTO userStatus (user_status, dateChanged, user) VALUES ('active'   , '2017-02-17 09:41:14', 4);
-INSERT INTO userStatus (user_status, dateChanged, user) VALUES ('recoMod'  , '2017-02-17 09:41:14', 5);
+    INSERT INTO auction (title, description, closeDate, initialValue, category_id, user_id) VALUES ('Mansão de luxo', 'Tem portas','2021-02-17 10:00:00', 500, 2, 1);
+    INSERT INTO auction (title, description, closeDate, initialValue, category_id, user_id) VALUES ('Dildo Ultra Dragon X', 'Larga o PePINo na saladeira, agora está na hora do Ultra Dragon X','2021-02-17 16:45:00', 2, 1, 3);
+    INSERT INTO auction (title, description, closeDate, initialValue, category_id, user_id) VALUES ('skin P90 CS:GO', 'Tem cores e stickers e formas','2021-02-17 09:41:14', 2, 1, 4);
+    INSERT INTO auction (title, description, closeDate, initialValue, category_id, user_id) VALUES ('Relogio CASIO', 'Tem horas? Já não vai precisar de fazer tal pergunta com o novo Relógio CASIO','2020-04-20 13:15:00', 2, 1, 5);
+    INSERT INTO auction (title, description, closeDate, initialValue, category_id, user_id) VALUES ('Ganda Bicha', 'Um cabo que serve para fazer variar a tensão da valuma da vela grande :oksign:','2021-02-17 09:00:15', 2, 1, 4);
 
-INSERT INTO "image" (path, alt) VALUES ('/website/src/img/Patilhas.jpg'   , 'Patilhas');
-INSERT INTO "image" (path, alt) VALUES ('/website/src/img/Risinhos.jpg'   , 'Risinhos');
-INSERT INTO "image" (path, alt) VALUES ('/website/src/img/Will.jpg'       , 'Will');
-INSERT INTO "image" (path, alt) VALUES ('/website/src/img/Standby.jpg'    , 'Standby');
-INSERT INTO "image" (path, alt) VALUES ('/website/src/img/motos.jpg'   , 'motos');
-INSERT INTO "image" (path, alt) VALUES ('/website/src/img/antiques.jpg'   , 'antiques');
-INSERT INTO "image" (path, alt) VALUES ('/website/src/img/phones.jpg'   , 'phones');
+    INSERT INTO auctionStatus (status, dateChanged, auction) VALUES ('ongoing', '2017-04-17 19:41:14', 1);
+    INSERT INTO auctionStatus (status, dateChanged, auction) VALUES ('ongoing', '2017-02-15 09:11:11', 2);
+    INSERT INTO auctionStatus (status, dateChanged, auction) VALUES ('closed' , '2017-05-17 07:41:14', 3);
+    INSERT INTO auctionStatus (status, dateChanged, auction) VALUES ('removed', '2017-09-18 11:23:00', 4);
+    INSERT INTO auctionStatus (status, dateChanged, auction) VALUES ('ongoing', '2018-02-17 09:41:14', 5);
 
-INSERT INTO "transaction" (value,date,description,sender_id,receiver_id,is_reserved,auction) VALUES (250  ,'2017-12-14 19:41:24', 'Watch auction', 1, 2, 'true', 1);
-INSERT INTO "transaction" (value,date,description,sender_id,receiver_id,is_reserved,auction) VALUES (1250 ,'2018-07-17 09:40:14', 'Car auction', 1, 3, 'true', 2);
-INSERT INTO "transaction" (value,date,description,sender_id,receiver_id,is_reserved,auction) VALUES (2250 ,'2018-04-23 15:41:54', 'Moto auction', 4, 2, 'false', 3);
-INSERT INTO "transaction" (value,date,description,sender_id,receiver_id,is_reserved,auction) VALUES (200  ,'2018-03-19 13:25:18', 'Guitar auction', 3, 2, 'true', 4);
+    INSERT INTO userStatus (status, dateChanged, user_id) VALUES ('active'   , '2017-02-17 09:41:14', 1);
+    INSERT INTO userStatus (status, dateChanged, user_id) VALUES ('active'   , '2017-02-17 09:41:14', 2);
+    INSERT INTO userStatus (status, dateChanged, user_id) VALUES ('moderator', '2017-02-17 09:41:14', 3);
+    INSERT INTO userStatus (status, dateChanged, user_id) VALUES ('active'   , '2017-02-17 09:41:14', 4);
+    INSERT INTO userStatus (status, dateChanged, user_id) VALUES ('recoMod'  , '2017-02-17 09:41:14', 5);
 
-INSERT INTO follows (user,category_id) VALUES (1,1);
-INSERT INTO follows (user,category_id) VALUES (2,1);
-INSERT INTO follows (user,category_id) VALUES (3,2);
-INSERT INTO follows (user,category_id) VALUES (4,3);
+    INSERT INTO "transaction" (value,date,description,sender_id,receiver_id,is_reserved,auction) VALUES (250  ,'2017-12-14 19:41:24', 'Watch auction', 1, 2, 'true', 1);
+    INSERT INTO "transaction" (value,date,description,sender_id,receiver_id,is_reserved,auction) VALUES (1250 ,'2018-07-17 09:40:14', 'Car auction', 1, 3, 'true', 2);
+    INSERT INTO "transaction" (value,date,description,sender_id,receiver_id,is_reserved,auction) VALUES (2250 ,'2018-04-23 15:41:54', 'Moto auction', 4, 2, 'false', 3);
+    INSERT INTO "transaction" (value,date,description,sender_id,receiver_id,is_reserved,auction) VALUES (200  ,'2018-03-19 13:25:18', 'Guitar auction', 3, 2, 'true', 4);
 
-INSERT INTO followsAuction (user,auction_id) VALUES (1,1);
-INSERT INTO followsAuction (user,auction_id) VALUES (1,3);
-INSERT INTO followsAuction (user,auction_id) VALUES (2,1);
-INSERT INTO followsAuction (user,auction_id) VALUES (3,2);
-INSERT INTO followsAuction (user,auction_id) VALUES (4,4);
+    INSERT INTO follows (user_id,category_id) VALUES (1,1);
+    INSERT INTO follows (user_id,category_id) VALUES (2,1);
+    INSERT INTO follows (user_id,category_id) VALUES (3,2);
+    INSERT INTO follows (user_id,category_id) VALUES (4,3);
 
-INSERT INTO AuctionImage (auction_id,image_id) VALUES (1,5);
-INSERT INTO AuctionImage (auction_id,image_id) VALUES (2,6);
-INSERT INTO AuctionImage (auction_id,image_id) VALUES (3,7);
+    INSERT INTO follows_auction (user_id,auction_id) VALUES (1,1);
+    INSERT INTO follows_auction (user_id,auction_id) VALUES (1,3);
+    INSERT INTO follows_auction (user_id,auction_id) VALUES (2,1);
+    INSERT INTO follows_auction (user_id,auction_id) VALUES (3,2);
+    INSERT INTO follows_auction (user_id,auction_id) VALUES (4,4);
 
-INSERT INTO bid (value, date, auction_id, user) VALUES (250, '2017-12-14 19:41:24', 1, 1);
-INSERT INTO bid (value, date, auction_id, user) VALUES (300, '2017-12-15 19:41:24', 1, 2);
-INSERT INTO bid (value, date, auction_id, user) VALUES (350, '2017-12-16 19:41:24', 1, 1);
-INSERT INTO bid (value, date, auction_id, user) VALUES (1250,'2018-12-14 19:41:24', 2, 1);
-INSERT INTO bid (value, date, auction_id, user) VALUES (550, '2018-12-30 19:41:24', 3, 1);
+    INSERT INTO auction_image (auction_id,image_id) VALUES (1,5);
+    INSERT INTO auction_image (auction_id,image_id) VALUES (2,6);
+    INSERT INTO auction_image (auction_id,image_id) VALUES (3,7);
 
-INSERT INTO review (stars,description,auction_id,user) VALUES (5,'Very Nice watch, works fine', 1, 1);
-INSERT INTO review (stars,description,auction_id,user) VALUES (3,'Medium Bycicle, expected more', 2, 2);
+    INSERT INTO bid (value, date, auction_id, user_id) VALUES (250, '2017-12-14 19:41:24', 1, 1);
+    INSERT INTO bid (value, date, auction_id, user_id) VALUES (300, '2017-12-15 19:41:24', 1, 2);
+    INSERT INTO bid (value, date, auction_id, user_id) VALUES (350, '2017-12-16 19:41:24', 1, 1);
+    INSERT INTO bid (value, date, auction_id, user_id) VALUES (1250,'2018-12-14 19:41:24', 2, 1);
+    INSERT INTO bid (value, date, auction_id, user_id) VALUES (550, '2018-12-30 19:41:24', 3, 1);
 
-INSERT INTO reportStatus (TYPE, dateChanged, oldStatus) VALUES ('notSeen','2017-12-14 19:41:24','notSeen');
-INSERT INTO reportStatus (TYPE, dateChanged, oldStatus) VALUES ('seen','2018-12-14 19:41:24','notSeen');
-INSERT INTO reportStatus (TYPE, dateChanged, oldStatus) VALUES ('closed','2018-07-12 19:41:24','seen';
+    INSERT INTO review (stars,description,auction_id,user_id) VALUES (5,'Very Nice watch, works fine', 1, 1);
+    INSERT INTO review (stars,description,auction_id,user_id) VALUES (3,'Medium Bycicle, expected more', 2, 2);
 
-INSERT INTO report (description, auction_id, user, status_id) VALUES ("Illegal Item", 1,1,1);
-INSERT INTO report (description, auction_id, user, status_id) VALUES ("Image does not correspond", 2,3,2);
+    INSERT INTO reportStatus (TYPE, dateChanged, oldStatus) VALUES ('notSeen','2017-12-14 19:41:24','notSeen');
+    INSERT INTO reportStatus (TYPE, dateChanged, oldStatus) VALUES ('seen','2018-12-14 19:41:24','notSeen');
+    INSERT INTO reportStatus (TYPE, dateChanged, oldStatus) VALUES ('closed','2018-07-12 19:41:24','seen');
 
-INSERT INTO bugReport (description,user,status_id) VALUES ("Shopping cart doesn't work", 1,1);
-INSERT INTO bugReport (description,user,status_id) VALUES ("can't create auction", 3,2);
+    INSERT INTO report (description, auction_id, user_id, status_id) VALUES ('Illegal Item', 1,1,1);
+    INSERT INTO report (description, auction_id, user_id, status_id) VALUES ('Image does not correspond', 2,3,2);
 
-INSERT INTO "admin" (username, password) VALUES ('guizinhos', 'KUFASLDFAIUQW12EQD8');
-INSERT INTO "admin" (username, password) VALUES ('estouNaOficina', 'KSAIOD3QSDAW12EQD8');
-INSERT INTO "admin" (username, password) VALUES ('Hentai4Ev3r', 'DSFOSHWQ33934SDLFEWF');
-INSERT INTO "admin" (username, password) VALUES ('exLata_deTinta', 'LIGFH893P2FS040W13');
+    INSERT INTO bugReport (description,user_id,status_id) VALUES ('Shopping cart does not work', 1,1);
+    INSERT INTO bugReport (description,user_id,status_id) VALUES ('cannot create auction', 3,2);
 
-INSERT INTO "notification" (title, bid, user, seen, type) VALUES ('Your bid was outdated', 1, 1, false, 'outdated_bid');
-INSERT INTO "notification" (title, bid, user, seen, type) VALUES ('Your auction has a new bid!', 1, 1, false, 'new_bid');
-INSERT INTO "notification" (title, bid, user, seen, type) VALUES ('You just won the auction', 3, 1, true, 'auction_won');
+    INSERT INTO "admin" (username, password) VALUES ('guizinhos', 'KUFASLDFAIUQW12EQD8');
+    INSERT INTO "admin" (username, password) VALUES ('estouNaOficina', 'KSAIOD3QSDAW12EQD8');
+    INSERT INTO "admin" (username, password) VALUES ('Hentai4Ev3r', 'DSFOSHWQ33934SDLFEWF');
+    INSERT INTO "admin" (username, password) VALUES ('exLata_deTinta', 'LIGFH893P2FS040W13');
