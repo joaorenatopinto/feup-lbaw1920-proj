f951001ca086a2895e670e5cc4b7b0a9771677d6
João Campos <joao.campos1221@gmail.com>
2020-04-19 16:32:04 +0100

    routes

--- website/routes/web.php
+++ website/routes/web.php
@@ -12,19 +12,17 @@
 */
 
 Route::get('/', 'Auth\LoginController@home');
-// Cards
+
+// User
+Route::get('user/{id}', 'CardController@show');
+Route::get('user/{id}/edit', 'CardController@edit');
+
+// Auctions
 Route::get('cards', 'CardController@list');
 Route::get('cards/{id}', 'CardController@show');
 
-// API
-Route::put('api/cards', 'CardController@create');
-Route::delete('api/cards/{card_id}', 'CardController@delete');
-Route::put('api/cards/{card_id}/', 'ItemController@create');
-Route::post('api/item/{id}', 'ItemController@update');
-Route::delete('api/item/{id}', 'ItemController@delete');
 
 // Authentication
-
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
