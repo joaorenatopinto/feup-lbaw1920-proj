31ae5d6537dc8de9f70f71f10add2ce1116bc58a
João Campos <joao.campos1221@gmail.com>
2020-04-20 19:18:02 +0100

    .

---
+++ website/app/Http/Controllers/HomepageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+class HomepageController extends Controller
+{
+    /**
+     * Shows homepage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show()
+    {
+      return view('pages.home');
+    }
+}
--- website/routes/web.php
+++ website/routes/web.php
@@ -11,20 +11,11 @@
 |
 */
 
+// Home
 Route::get('/', 'Auth\LoginController@home');
-// Cards
-Route::get('cards', 'CardController@list');
-Route::get('cards/{id}', 'CardController@show');
-
-// API
-Route::put('api/cards', 'CardController@create');
-Route::delete('api/cards/{card_id}', 'CardController@delete');
-Route::put('api/cards/{card_id}/', 'ItemController@create');
-Route::post('api/item/{id}', 'ItemController@update');
-Route::delete('api/item/{id}', 'ItemController@delete');
+Route::get('home', 'HomepageController@show');
 
 // Authentication
-
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
