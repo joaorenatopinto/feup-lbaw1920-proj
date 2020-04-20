b74e099281d59330b14c37a7cbd76eb7e47e0ffb
leonardofmoura <leofmoura17@gmail.com>
2020-04-21 00:46:53 +0100

    Added auction cards to user profile page

--- website/resources/views/pages/profile.blade.php
+++ website/resources/views/pages/profile.blade.php
@@ -50,28 +50,9 @@
     </div>
 
   </div>
-  <div class="card m-3 mx-auto">
-    <div class="row no-gutters">
-      <img class="card-img col-md-5" src="img/Harley-Davidson-FXDR-114_Fernando-M-1.jpg" alt="Card image cap">
-      <div class="card-body col-md-7 p-3">
-        <h5 class="card-title">Harley Davidson</h5>
-        <p class="card-text">2008, 1600cc, 6vel, many extras and in immaculate condition. Recent overhaul and new tires.</p>
-        <p class="card-text">00d 22h 25m 17s left</p>
-        <h3><a href="auction.php" class="btn btn-primary" style="width: 10rem;">BID NOW</a> <span class="badge">1337.69€</span></h3>
-      </div>
-    </div>
-  </div>
-  <div class="card m-3 mx-auto">
-    <div class="row no-gutters">
-      <img class="card-img col-md-5" src="img/yamaha.jpg" alt="Card image cap">
-      <div class="card-body col-md-7 p-3">
-        <h5 class="card-title">Yamaha R10M</h5>
-        <p class="card-text">2018, 1600cc, 5vel, many extras and new tires. Recent overhaul.</p>
-        <p class="card-text">10d 20h 35m 12s left</p>
-        <h3><a href="auction.php" class="btn btn-primary" style="width: 10rem;">BID NOW</a> <span class="badge">1337.69€</span></h3>
-      </div>
-    </div>
-  </div>
+  
+  @each('partials.auctionCard', $user->auctions, 'auction')
+
 </div>
 
 </div>
