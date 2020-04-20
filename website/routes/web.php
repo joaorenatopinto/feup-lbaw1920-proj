514ce50ac0ea1f570af403d034b74288c09c956a
João Campos <joao.campos1221@gmail.com>
2020-04-20 20:07:36 +0100

    .

--- website/routes/web.php
+++ website/routes/web.php
@@ -21,3 +21,6 @@ Route::post('login', 'Auth\LoginController@login');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register');
+
+// Auctions
+Route::get('auction/{id}', 'AuctionController@show');
