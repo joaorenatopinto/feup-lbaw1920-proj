a277b9dcacf75cf7fc5ba13b5f6df4673a20eaca
João Campos <joao.campos1221@gmail.com>
2020-04-25 20:14:16 +0100

    .

--- website/app/Policies/AuctionPolicy.php
+++ website/app/Policies/AuctionPolicy.php
@@ -4,6 +4,7 @@ namespace App\Policies;
 
 use App\User;
 use App\Auction;
+use App\Bid;
 
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Illuminate\Support\Facades\Auth;
@@ -20,5 +21,17 @@ class AuctionPolicy
   public function edit(User $user, Auction $auction)
   {
     return $user->id === $auction->user_id;
+  }
+
+  public function bid(User $user, Auction $auction)
+  {
+    return $user->id !== $auction->user_id;
+  }
+
+  public function review(User $user, Auction $auction)
+  {
+    $price = Bid::where('auction_id', $auction->id)->max('value');
+    $winner_id = Bid::where('auction_id', $auction->id)->where('value', $price)->first();
+    return $user->id !== $winner_id;
   }
 }
