19d621a8b2d1c82666696c170600dd8bb6ea17db
leonardofmoura <leofmoura17@gmail.com>
2020-05-31 17:07:25 +0100

    Added permitions to access administration aread

--- website/app/Http/Controllers/AdminController.php
+++ website/app/Http/Controllers/AdminController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Admin;
 use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
 
   public function users()
   {
-    $users = User::paginate(5);
+    if (Auth::guard('admin')->check()) {
+      $users = User::paginate(5);
 
-    return view('admin.users', ['users' => $users]);
+      return view('admin.users', ['users' => $users]);
+    }
+
+    return redirect(route('home'));
   }
 }
