6b979ca20974f148634023a43978acd7b0c0d662
leonardofmoura <leofmoura17@gmail.com>
2020-06-03 15:51:10 +0100

    Refactored category page view

--- website/resources/views/partials/auctionCard.blade.php
+++ website/resources/views/partials/auctionCard.blade.php
@@ -1,12 +1,10 @@
-<div class="card mx-auto m-3">
-    <div class="row no-gutters">
-        <img class="card-img col-md-5" src="{{App\Image::where('auction_id',$auction->id)->first()->path}}" alt="Card image cap">
-        <div class="card-body col-md-7 p-3">
-            <h5 class="card-title">  {{ $auction->title }} </h5>
-            <p class="card-text"> {{ $auction->description }}</p>
-            <p class="card-text"> {{ $auction->closedate }} </p>
-            <h3><a href="{{route('auction', [$id = $auction->id])}}" class="btn btn-primary" style="width: 10rem;">BID NOW</a> <span
+<div class="col-sm-4 p-0 card">
+    <img class="card-img-top" src="{{App\Image::where('auction_id',$auction->id)->first()->path}}" height="200" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">  {{ $auction->title }} </h5>
+        <p class="card-text"> {{ $auction->description }}</p>
+        <p class="card-text mt-auto"> {{ $auction->closedate }} </p>
+        <h3><a href="{{route('auction', [$id = $auction->id])}}" class="btn btn-primary" style="width: 10rem;">BID NOW</a> <span
                     class="badge">{{ $auction->getHighestBid() }}€</span></h3>
-        </div>
     </div>
 </div>
